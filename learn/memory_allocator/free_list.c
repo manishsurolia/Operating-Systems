ab3509c58d6887b068dc28c9ba9a3749bbbeaf96
Manish <manishsurolia@gmail.com>
2021-05-15 09:44:17 +0530

    some corrections in mem allocator

--- learn/memory_allocator/free_list.c
+++ learn/memory_allocator/free_list.c
@@ -1,25 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/mman.h>
-#include <errno.h>
-#include <string.h>
-
-#define HEAP_SIZE 4096
-#define MAGIC_HEADER 0x12ABCD34
-
-typedef struct __header_t {
-    int magic;
-    int size;
-} header_t;
-
-typedef struct __node_t {
-    int size;
-    struct __node_t *next;
-} node_t;
+#include "free_list.h"
 
 void * get_heap(void) {
-    node_t *heap = mmap(NULL, HEAP_SIZE, (PROT_READ|PROT_WRITE),
-                  (MAP_ANON | MAP_PRIVATE), -1, 0);
+    node_t *heap = NULL;
+    heap = mmap(NULL, HEAP_SIZE, (PROT_READ|PROT_WRITE),
+            (MAP_ANON | MAP_PRIVATE), -1, 0);
     if (heap == MAP_FAILED) {
         perror(strerror(errno));
         exit (1);
@@ -32,23 +16,41 @@ void * get_heap(void) {
 
 void * my_malloc(int size)
 {
-    static node_t *heap;
+    static node_t *head;
+    static int allocated; /* Only one time allocation */
     void * ret;
+    int requested_size = sizeof(header_t) + size;
 
-    if (!heap) {
-        heap = get_heap();
+    if (!allocated) {
+        head = get_heap();
+        allocated = 1;
     }
 
     /* First fit Implementation */ 
-    while (heap) {
-        if ((sizeof(node_t) + heap->size) >= (sizeof(header_t) + size)) {
-            ret = heap + sizeof(node_t);  
+    while (head) {
+        int available_chunk_size = sizeof(node_t) + head->size;
+        if (requested_size < available_chunk_size) {
+
+            ret = head;
             ((header_t *)ret)->magic = MAGIC_HEADER;
             ((header_t *)ret)->size = size;
-            heap->next = ret + sizeof(header_t) + size;
+
+            if ((available_chunk_size - requested_size) >= sizeof(node_t)) {
+                head = (void *)head + requested_size;
+                head->size = available_chunk_size - requested_size -
+                             sizeof(node_t);
+                head->next = NULL;
+            } else {
+                head = NULL;
+            }
             return (ret + sizeof(header_t));
         }
-        heap = heap->next;
+
+        if (head->next) {
+            head = head->next;
+        } else {
+            break;
+        }
     }
     return  NULL;
 }
@@ -57,12 +59,35 @@ int main(int argc, char **argv)
 {
     void *p;
     while(1) {
-        p = my_malloc(1000); 
-        if (!p) {
+        if (!my_malloc(1000)) {
+            printf("Got NULL, no more sufficient memory available in heap\n");
             break;
         } else {
             printf("1000 bytes allocted.\n");
         }
+
     }
+
+    while(1) {
+        if (!my_malloc(50)) {
+            printf("Got NULL, no more sufficient memory available in heap\n");
+            break;
+        } else {
+            printf("50 bytes allocted.\n");
+        }
+
+    }
+
+    while(1) {
+        if (!my_malloc(1)) {
+            printf("Got NULL, no more sufficient memory available in heap\n");
+            break;
+        } else {
+            printf("1 byte allocted.\n");
+        }
+
+    }
+
+
     return 0;
 }
---
+++ learn/memory_allocator/free_list.h
@@ -0,0 +1,23 @@
+#ifndef FREE_LIST_H
+#define FREE_LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <string.h>
+
+#define HEAP_SIZE 4096
+#define MAGIC_HEADER 0x12ABCD34
+
+typedef struct __header_t {
+    int magic;
+    int size;
+} header_t;
+
+typedef struct __node_t {
+    int size;
+    struct __node_t *next;
+} node_t;
+
+#endif /* FREE_LIST_H */
