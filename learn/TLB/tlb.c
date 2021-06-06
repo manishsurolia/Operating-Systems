6b232a57a415f9b16ba995f23a5453be7b0babe4
Manish <manishsurolia@gmail.com>
2021-06-06 18:37:20 +0530

    tlb usage program

--- learn/TLB/tlb.c
+++ learn/TLB/tlb.c
@@ -15,6 +15,9 @@
  * access time per page' varies with more number of pages because TLB has a
  * limit(32/64/128 entries max).
  *
+ * Note : This program runs only on a single core on the system. Achieved with
+ * 'sched_setaffinity' system call.
+ *
  * For complete details about paging and TLB see the related README files in the
  * same directory here.
  *
@@ -25,26 +28,39 @@
  * involvement of any TLB here.
  */
 
+
+#define _GNU_SOURCE
+#include <sched.h> /* To schedule this program on a single core */
+
 #include <stdio.h>
 #include <sys/time.h>
 #include <time.h>
 #include <stdint.h>
 
 #define BILLION 1000000000L
-#define NUMPAGES 32 // Number of pages used by our array 'arr'
-#define PAGESIZE 4096 // Taken from this command of linux 'getconf PAGE_SIZE'
+#define NUMPAGES 32 /* Number of pages used by our array 'arr' */
+#define PAGESIZE 4096 /* Taken from this command of linux 'getconf PAGE_SIZE' */
 #define ARR_SIZE ((PAGESIZE / sizeof(int)) * NUMPAGES)
-#define REPEAT_FACTOR 10000000
+#define REPEAT_FACTOR 10000000 /* To update all pages this many times */
 
 int arr[ARR_SIZE];
 
 #pragma GCC optimize ("O0")
 int main(int argc, char **argv)
 {
-    unsigned int jump = PAGESIZE / sizeof(int); // To jump from page to the next
+    unsigned int jump = PAGESIZE / sizeof(int); /* To jump one page to next */
     struct timespec tstart={0,0}, tend={0,0};
     uint64_t total = 0;
-    uint64_t average = 0; //Average time(in ns), per page access
+    uint64_t average = 0; /* Average time(in ns), per page access */
+
+    /*
+     * Forcing this process to run on a single processer, because each processor
+     * has a TLB with it, and we want to test a single processor TLB behaviour.
+     */
+    cpu_set_t my_set;
+    CPU_ZERO(&my_set);
+    CPU_SET(0, &my_set);
+    sched_setaffinity(0, sizeof(cpu_set_t), &my_set);
 
     /*
      * We want to update each page in the array REPEAT_FACTOR times.
@@ -64,7 +80,7 @@ int main(int argc, char **argv)
         tstart.tv_sec = tstart.tv_nsec = tend.tv_sec = tend.tv_nsec = 0;
     }
     average = (total / REPEAT_FACTOR) / NUMPAGES;
-    printf("\nAverage Time (Per page access) : %llu nanoseconds\n",
+    printf("Average Time (Per page access) : %llu nanoseconds\n",
             (long long unsigned int)average);
 
     return 0;
